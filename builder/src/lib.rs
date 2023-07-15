53840a7bce1278fe06ddeac4e3bbbe2133a007d8
fruscianteee <redhot1205@gmail.com>
2023-07-15 19:57:54 +0900

    pass test02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,14 +1,39 @@
 use proc_macro::TokenStream;
 use quote::*;
-use syn::DeriveInput;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let input: DeriveInput = syn::parse(input).unwrap();
+    let input: syn::ItemStruct = syn::parse(input).unwrap();
     let name = input.ident;
+    let mut fields = vec![];
+    let mut default_fields = vec![];
+    for field in input.fields {
+        let ty = field.ty;
+        let ident = field.ident;
+        let x = quote! {
+            #ident: Option<#ty>
+        };
+        fields.push(x);
+
+        let y = quote! {
+            #ident: None
+        };
+        default_fields.push(y);
+    }
+
+    let builer_name = format_ident!("{}Builder", name);
     let tokens = quote! {
+
+        pub struct #builer_name {
+            #(#fields),*
+        }
+
         impl #name {
-            pub fn builder() { }
+            pub fn builder() -> #builer_name {
+                #builer_name {
+                    #(#default_fields),*
+                }
+            }
         }
     };
     tokens.into()
