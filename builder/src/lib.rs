a8b7457f3aeec8e6f92c87e04f18bd6fd0868b17
skanehira <sho19921005@gmail.com>
2023-07-15 19:18:43 +0900

    chore: pass the 02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,15 @@
 use proc_macro::TokenStream;
+use quote::*;
+use syn::DeriveInput;
 
 #[proc_macro_derive(Builder)]
-pub fn derive(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
+pub fn derive(input: TokenStream) -> TokenStream {
+    let input: DeriveInput = syn::parse(input).unwrap();
+    let name = input.ident;
+    let tokens = quote! {
+        impl #name {
+            pub fn builder() { }
+        }
+    };
+    tokens.into()
 }
