834de9c84702cabaff16eebab4822c88232a3699
skanehira <sho19921005@gmail.com>
2023-10-14 19:48:29 +0900

    refactor

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,23 +2,40 @@ use proc_macro::TokenStream;
 use quote::*;
 use syn::Type;
 
-fn get_typepath_in_option(ty: &Type) -> Option<Type> {
-    match ty {
-        Type::Path(ref typepath) => {
-            let Some(seg) = typepath.path.segments.first() else { return None };
-            if seg.ident == "Option" {
-                if let syn::PathArguments::AngleBracketed(ref inner_ty) = seg.arguments {
-                    if let Some(syn::GenericArgument::Type(ty)) = inner_ty.args.first() {
-                        return Some(ty.clone());
-                    }
+// Option<syn::Type> から syn::Type を取り出す
+// Type::Path(
+//     TypePath {
+//         qself: None,
+//         path: Path {
+//             segments: [
+//                 PathSegment {
+//                     ident: "Option",
+//                     arguments: PathArguments::AngleBracketed(
+//                         AngleBracketedGenericArguments {
+//                             args: [
+//                                 GenericArgument::Type(
+//                                     ... // ここに `Option<T>`のTが入る
+//                                 ),
+//                             ],
+//                         },
+//                     ),
+//                 },
+//             ],
+//         },
+//     },
+// )
+fn get_type_in_option(ty: &Type) -> Option<Type> {
+    if let Type::Path(ref typepath) = ty {
+        let Some(seg) = typepath.path.segments.first() else { return None };
+        if seg.ident == "Option" {
+            if let syn::PathArguments::AngleBracketed(ref inner_ty) = seg.arguments {
+                if let Some(syn::GenericArgument::Type(ty)) = inner_ty.args.first() {
+                    return Some(ty.clone());
                 }
-                None
-            } else {
-                None
             }
         }
-        _ => None,
     }
+    None
 }
 
 #[proc_macro_derive(Builder)]
@@ -26,16 +43,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let input: syn::ItemStruct = syn::parse(input).unwrap();
     let name = input.ident;
     let mut fields = vec![];
-    let mut default_fields = vec![];
     let mut methods = vec![];
     // Commandのフィールド
-    let mut assign_fields = vec![];
+    let mut fields_for_build = vec![];
     // Command を文字列に変換して、lowercaseにして、format_ident!()でASTに戻す
     let lower_name = format_ident!("{}", name.to_string().to_lowercase());
 
     for field in input.fields {
         let mut ty = field.ty;
-        let inner_ty = get_typepath_in_option(&ty);
+        let inner_ty = get_type_in_option(&ty);
         if let Some(ref inner_ty) = inner_ty {
             ty = inner_ty.clone();
         }
@@ -52,7 +68,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         };
 
-        assign_fields.push(assign_field);
+        fields_for_build.push(assign_field);
 
         methods.push(quote! {
             fn #field_name(&mut self, #field_name: #ty) -> &mut Self {
@@ -61,19 +77,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         });
 
-        let x = quote! {
+        let field = quote! {
             #field_name: Option<#ty>
         };
-        fields.push(x);
-
-        let y = quote! {
-            #field_name: None
-        };
-        default_fields.push(y);
+        fields.push(field);
     }
 
     let builder_name = format_ident!("{}Builder", name);
     let tokens = quote! {
+        #[derive(Default)]
         pub struct #builder_name {
             #(#fields),*
         }
@@ -82,9 +94,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #name {
             pub fn builder() -> #builder_name {
                 // #builder_name is the struct name(CommandBuilder)
-                #builder_name {
-                    #(#default_fields),*
-                }
+                #builder_name::default()
             }
         }
 
@@ -94,7 +104,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
             fn build(&self) -> Result<#name, Box<dyn std::error::Error>> {
                 let #lower_name = #name {
-                    #(#assign_fields),*
+                    #(#fields_for_build),*
                 };
                 Ok(#lower_name)
             }
